cb260fac35088ae35790775406f8308da858c212
3001Aswini <123323057+3001Aswini@users.noreply.github.com>
2024-06-30 21:08:43 +0530

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -3,15 +3,14 @@ import dashboard
 import stats
 import login
 import home
-# Main page layout
+
 st.set_page_config(page_title="PollVue", layout="wide")
 
 
-# Navigation
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox("Select a page:", ["Home","Login/Signup", "News","Statistics"])
 
-# Display selected page
+
 if page == "Home":
     home.main()
 elif page == "News":
