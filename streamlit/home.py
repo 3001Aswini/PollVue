ffa973add278277125ce302892042135dcee79ea
3001Aswini <123323057+3001Aswini@users.noreply.github.com>
2024-06-30 21:09:47 +0530

    Update home.py

--- streamlit/home.py
+++ streamlit/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.markdown("<h1 style='text-align: center;'>PollVue</h1>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center;'>Public opinion lens!</h4>", unsafe_allow_html=True)
-    # List of image URLs related to politics
+   
     image_urls = [
         "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*hbEgvBf99ZyrgZ28iD0BnQ.jpeg",
         "https://media.licdn.com/dms/image/D5612AQHEeG5WXk4GQA/article-cover_image-shrink_720_1280/0/1684161928638?e=1725494400&v=beta&t=XcA1QZ8xJ7gYuxzK_kWvg7p0XZZrylwwfjew5kDRsGg",
@@ -11,15 +11,14 @@ def main():
         
     ]
     
-    # Initialize the session state for the current image index
     if 'current_image' not in st.session_state:
         st.session_state.current_image = 0
 
-    # Display the current image
+    
     current_image = st.session_state.current_image
     st.image(image_urls[current_image], use_column_width=True)
 
-    # Buttons to navigate through images
+
     col1, col2, col3 = st.columns([1, 6, 1])
     
     with col1:
@@ -30,8 +29,8 @@ def main():
         if st.button("→"):
             st.session_state.current_image = (current_image + 1) % len(image_urls)
 
-    # Add a "Get Started" button below the image slider
-    st.markdown("<br>", unsafe_allow_html=True)  # Add some space
+   
+    st.markdown("<br>", unsafe_allow_html=True)  
     
     
 
